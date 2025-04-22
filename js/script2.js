0f3b5c8bf5756594df9a9f127751c0aa7e52a0d0
Anakha S <144688034+anakhavaishakham2005@users.noreply.github.com>
2025-04-22 08:46:16 +0530

    did page transition

--- js/script2.js
+++ js/script2.js
@@ -1,26 +1,24 @@
-document.getElementById("create-recipe-form").addEventListener("submit", function (e) {
-    e.preventDefault(); // stop page reload
-  
-    const name = document.getElementById("recipe-name").value;
-    const image = document.getElementById("recipe-image").files[0];
-    const ingredients = document.getElementById("recipe-ingredients").value.split(",");
-    const instructions = document.getElementById("recipe-instructions").value.split(".");
-    const nutrition = document.getElementById("recipe-nutrition").value.split(",");
-  
-    const reader = new FileReader();
-    reader.onload = function (event) {
-      const newRecipe = {
-        name,
-        image: event.target.result, // base64 string of image
-        ingredients,
-        instructions,
-        nutrition
-      };
-      localStorage.setItem("newRecipe", JSON.stringify(newRecipe));
-      window.location.href = "new.html";
-    };
-    reader.readAsDataURL(image); // convert image to base64
-  });
-  
 
-js/script2.js
+document.getElementById("create-recipe-form").addEventListener("submit", function (e) {
+  e.preventDefault(); // stop page reload
+
+  const name = document.getElementById("recipe-name").value;
+  const image = document.getElementById("recipe-image").files[0];
+  const ingredients = document.getElementById("recipe-ingredients").value.split(",");
+  const instructions = document.getElementById("recipe-instructions").value.split(".");
+  const nutrition = document.getElementById("recipe-nutrition").value.split(",");
+
+  const reader = new FileReader();
+  reader.onload = function (event) {
+    const newRecipe = {
+      name,
+      image: event.target.result, // base64 string of image
+      ingredients,
+      instructions,
+      nutrition
+    };
+    localStorage.setItem("newRecipe", JSON.stringify(newRecipe));
+    window.location.href = "new.html";
+  };
+  reader.readAsDataURL(image); // convert image to base64
+});
